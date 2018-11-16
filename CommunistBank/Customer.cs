cf1963e91bc9631cb515e6a2e5583829f6ba227f
Elliott Lie <Elliott.DTK.Lie@lystkom.se>
2018-11-16 23:43:24 +0100

    Add project files.

---
+++ CommunistBank/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElliottCommunistBank
+{
+
+    class Customer
+    {
+
+        public int balance { get; set; }
+        public string name { get; set; }
+        public List<Transaction> transactionsList { get; set; } = new List<Transaction>();
+
+        public static void ShowCustomer(int iCount)
+        {
+            // iCount + 1 because of user interface. List shows first customer as number 0, but we want
+            // want the user to see the first customer as number 1
+            Console.WriteLine("Customer " + (iCount + 1) + ": " + Program.customerList[iCount].name);
+        }
+
+        public static void RegisterTransaction(int money, Customer customer)
+        {
+            customer.balance += money; // adds the money to customers balance
+            Transaction transaction = new Transaction();// creates an transaction object that registers money and time
+            transaction.transactionMoney = money;
+            transaction.transactionTime = DateTime.Now.ToShortTimeString();
+            customer.transactionsList.Add(transaction); // saves the current transaction to the customer transaction list
+        }
+
+    }
+
+
+}
---
+++ CommunistBank/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElliottCommunistBank
+{
+    class Transaction
+    {
+        public string transactionTime { get; set; }
+        public int transactionMoney { get; set; }
+    }
+}
+
